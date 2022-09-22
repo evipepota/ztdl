4885b741ad1477f6efdb11f480da8141eb9cf99f
evipepota <meimuyuta@gmail.com>
2022-09-23 02:19:54 +0900

    ch02

--- src/dl/ch02/logic.rs
+++ src/dl/ch02/logic.rs
@@ -1,7 +1,7 @@
 use ndarray::Array1;
 
 #[allow(dead_code)]
-pub fn and(x1: f32, x2: f32) -> bool {
+pub fn and(x1: f64, x2: f64) -> bool {
     let w1 = 0.5;
     let w2 = 0.5;
     let theta = 0.7;
@@ -10,29 +10,10 @@ pub fn and(x1: f32, x2: f32) -> bool {
 }
 
 #[allow(dead_code)]
-pub fn and2(x1: f32, x2: f32) -> bool {
-    let x: Array1<f32> = ndarray::arr1(&[x1, x2]);
-    let w: Array1<f32> = ndarray::arr1(&[0.5, 0.5]);
+pub fn and2(x1: f64, x2: f64) -> bool {
+    let x: Array1<f64> = ndarray::arr1(&[x1, x2]);
+    let w: Array1<f64> = ndarray::arr1(&[0.5, 0.5]);
     let b = -0.7;
     let tmp = (w * x).sum() + b;
     tmp > 0.0
-}
-
-#[cfg(test)]
-pub mod tests {
-    use crate::dl::ch02::logic::{and, and2};
-    #[test]
-    pub fn test_and() {
-        assert!(!and(0.0, 0.0));
-        assert!(!and(1.0, 0.0));
-        assert!(!and(0.0, 1.0));
-        assert!(and(1.0, 1.0));
-    }
-    #[test]
-    pub fn test_and2() {
-        assert!(!and2(0.0, 0.0));
-        assert!(!and2(1.0, 0.0));
-        assert!(!and2(0.0, 1.0));
-        assert!(and2(1.0, 1.0));
-    }
 }
