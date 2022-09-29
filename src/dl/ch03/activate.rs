66c32d32ed785a8406e803c58be0c5583e653566
evipepota <meimuyuta@gmail.com>
2022-09-29 09:25:21 +0900

    ch03

--- src/dl/ch03/activate.rs
+++ src/dl/ch03/activate.rs
@@ -1,15 +1,18 @@
-use ndarray::Array1;
+use ndarray::{Array1, Array2};
 
 #[allow(dead_code)]
 pub fn step_f(x: &Array1<f64>) -> Array1<f64> {
-    x.iter()
-        .map(|&v| if v <= 0.0 { 0.0 } else { 1.0 })
-        .collect()
+    x.iter().map(|&v| if v <= 0.0 { 0.0 } else { 1.0 }).collect()
 }
 
 #[allow(dead_code)]
 pub fn sigmoid_f(x: &Array1<f64>) -> Array1<f64> {
     x.iter().map(|&v| 1.0 / (1.0 + (-v).exp())).collect()
+}
+
+#[allow(dead_code)]
+pub fn sigmoid_batch_f(x: &Array2<f64>) -> Array2<f64> {
+    x.map(|v| 1.0 / (1.0 + (-(*v)).exp()))
 }
 
 #[allow(dead_code)]
@@ -28,4 +31,25 @@ pub fn softmax_f(x: &Array1<f64>) -> Array1<f64> {
         sum += (i - c).exp();
     }
     x.iter().map(|&v| (v - c).exp() / sum).collect()
+}
+
+#[allow(dead_code)]
+pub fn softmax_batch_f(x: &Array2<f64>) -> Array2<f64> {
+    // muzusugi te kitanai code ni naattimatta... ze...
+    let mut res = x.clone();
+    for i in 0..x.nrows() {
+        let b = x.row(i);
+        let mut c = 0.0;
+        for i in b {
+            c = f64::max(c, *i);
+        }
+        let mut sum = 0.0;
+        for i in b {
+            sum += (i - c).exp();
+        }
+        for j in 0..x.ncols() {
+            res[[i, j]] = (x[[i, j]] - c).exp() / sum;
+        }
+    }
+    res
 }
--- src/dl/ch03/threenet.rs
+++ src/dl/ch03/threenet.rs
@@ -1,10 +1,10 @@
-use std::collections::HashMap;
-
+use super::activate::sigmoid_batch_f;
+use super::activate::sigmoid_f;
+use super::activate::softmax_batch_f;
+use super::activate::softmax_f;
 use ndarray::Array1;
 use ndarray::Array2;
-
-use super::activate::sigmoid_f;
-use super::activate::softmax_f;
+use std::collections::HashMap;
 
 type Wb = (HashMap<String, Array1<f64>>, HashMap<String, Array2<f64>>);
 
@@ -12,14 +12,8 @@ type Wb = (HashMap<String, Array1<f64>>, HashMap<String, Array2<f64>>);
 pub fn init_net() -> Wb {
     let mut map_b: HashMap<String, Array1<f64>> = HashMap::new();
     let mut map_w: HashMap<String, Array2<f64>> = HashMap::new();
-    map_w.insert(
-        "w1".to_string(),
-        ndarray::arr2(&[[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]),
-    );
-    map_w.insert(
-        "w2".to_string(),
-        ndarray::arr2(&[[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]),
-    );
+    map_w.insert("w1".to_string(), ndarray::arr2(&[[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]));
+    map_w.insert("w2".to_string(), ndarray::arr2(&[[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]));
     map_w.insert("w3".to_string(), ndarray::arr2(&[[0.1, 0.3], [0.2, 0.4]]));
     map_b.insert("b1".to_string(), ndarray::arr1(&[0.1, 0.2, 0.3]));
     map_b.insert("b2".to_string(), ndarray::arr1(&[0.1, 0.2]));
@@ -45,4 +39,14 @@ pub fn predict(net: Wb, x: Array1<f64>) -> Array1<f64> {
     let z2 = sigmoid_f(&a2);
     let a3 = z2.dot(&net.1["w3"]) + &net.0["b3"];
     softmax_f(&a3)
+}
+
+#[allow(dead_code)]
+pub fn predict_batch(net: Wb, x: Array2<f64>) -> Array2<f64> {
+    let a1 = x.dot(&net.1["w1"]) + &net.0["b1"];
+    let z1 = sigmoid_batch_f(&a1);
+    let a2 = z1.dot(&net.1["w2"]) + &net.0["b2"];
+    let z2 = sigmoid_batch_f(&a2);
+    let a3 = z2.dot(&net.1["w3"]) + &net.0["b3"];
+    softmax_batch_f(&a3)
 }
